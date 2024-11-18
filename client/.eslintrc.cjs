876d985cdb709cf177039ca19c4322c459311dc6
pedrojmir <pedrojmir@gmail.com>
2024-11-18 15:41:22 -0500

    Added Redux for state management, refactored code to use Redux for the basket states instead of React Context. Also updated lint.

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
-  rules: {
+    rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
